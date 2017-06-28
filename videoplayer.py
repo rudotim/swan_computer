3b65b897e673a662a35b70a2f2411eaba79c427f
tdawg <trudowski@gmail.com>
2017-06-28 10:21:32 -0400

    - added video playing capabilities though this is untested since it can only run on the RPI

--- videoplayer.py
+++ videoplayer.py
@@ -1,7 +1,11 @@
 
 #from omxplayer import OMXPlayer
 from time import sleep
+
 from playsound import playsound
+import pygame.time
+from __builtin__ import False
+
 
 class videoplayer:
     
@@ -12,6 +16,32 @@ class videoplayer:
     def playVideo(self, videoFilePath):
         print "playing video " + videoFilePath
         
+        FPS = 60
+
+        clock = pygame.time.Clock()
+        movie = pygame.movie.Movie(videoFilePath)
+        screen = pygame.display.set_mode(movie.get_size())
+        movie_screen = pygame.Surface(movie.get_size()).convert()
+        
+        movie.set_display(movie_screen)
+        movie.play()
+                
+        playing = True
+        while playing:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == 13:   # enter
+                        print "enter!"
+                        movie.stop()
+                        playing = False
+                                        
+                if event.type == pygame.QUIT:
+                    movie.stop()
+                    playing = False
+        
+            screen.blit(movie_screen,(0,0))
+            pygame.display.update()
+            clock.tick(FPS)
         # This will start an `omxplayer` process, this might
         # fail the first time you run it, currently in the
         # process of fixing this though.
