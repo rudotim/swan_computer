50111442d3d817c1c5442df786ea4340131e0438
Tim <trudowski@gmail.com>
2020-11-19 21:24:15 -0500

    - fixed ctrl-c issue

--- swan.py
+++ swan.py
@@ -3,113 +3,117 @@
 import sys, getopt
 
 #from gamecontroller import gamecontroller
+from threading import Thread
+
 from lost import gamecontroller
 
 from lost.swanengine import SwanEngine
 import traceback
 
 import signal
-    
-#global sock_comms
-#global threadSock
-#global lost
-sock_comms = None
-threadSock = None
-lost = None
+
+from web.webserver import webserver
 
 
-def main( argv ):
-    
-    global sock_comms
-    global threadSock
-    global lost
-    
-    port = 7777
-    
-    using_pi = False
-    cmd_string = 'swan.py [-p <port>]'
-    try:
-        opts, args = getopt.getopt(argv, "h", ["pi"])
-    except getopt.GetoptError:
-        print('error: {0}'.format(cmd_string))
-        sys.exit(2)
-    for opt, arg in opts:
-        print (opt)
-        if opt == '-h':
-            print('swan.py [-p <port>]')
-            sys.exit()
-        elif opt in "-p":
-            port = opt
-        elif opt in "--pi":
-            using_pi = True
-    
-    signal.signal(signal.SIGINT, signal_handler)
+class Swan:
 
-    #controller = gamecontroller.gamecontroller(using_pi)
+    def __init__(self, argv):
 
-    #sock_comms = socket_comms('/tmp/uds_socket', controller);
-    #threadSock = Thread(target=sock_comms.start, args=[])
-    #threadSock.start()
+        self.parse_parameters(argv)
 
-    #lost = lostengine(controller, quitApp );
-    #lost.init( using_pi )
+        self.controller = None
+        self.server = None
+        self.lost = None
+
+        self.port = 7777
+
+        self.using_pi = False
+
+    def parse_parameters(self, argv):
+        cmd_string = 'swan.py [-p <port>]'
+        try:
+            opts, args = getopt.getopt(argv, "h", ["pi"])
+        except getopt.GetoptError:
+            print('error: {0}'.format(cmd_string))
+            sys.exit(2)
+        for opt, arg in opts:
+            print (opt)
+            if opt == '-h':
+                print('swan.py [-p <port>]')
+                sys.exit()
+            elif opt in "-p":
+                port = opt
+            elif opt in "--pi":
+                using_pi = True
+
+    def start(self):
+        #controller = gamecontroller.gamecontroller(using_pi)
+
+        #sock_comms = socket_comms('/tmp/uds_socket', controller);
+        #threadSock = Thread(target=sock_comms.start, args=[])
+        #threadSock.start()
+
+        #lost = lostengine(controller, quitApp );
+        #lost.init( using_pi )
 
 
-    #threadUI = Thread(target=lost.init, args=[ using_pi ])
-    #threadUI.start()
-
-    try:
-        controller = gamecontroller.gamecontroller(using_pi)
-         
-         # start web server in separate thread
-         #server = webserver(port, controller)
-         #thread = Thread(target = server.start, args = [])
-         #thread.start()
-         
-         #sock_comms = socket_comms( '/tmp/uds_socket' );
-         #threadSock = Thread(target = sock_comms.start, args = [])
-         #threadSock.start()
-        lost = SwanEngine(controller, quitApp)
-        print("start it")
-        lost.init( using_pi )
-        #threadUI = Thread(target = lost.init, args = [ using_pi ])
+        #threadUI = Thread(target=lost.init, args=[ using_pi ])
         #threadUI.start()
 
+        try:
+            self.controller = gamecontroller.gamecontroller(self.using_pi)
 
-         # start lost engine
-         #lost = lostengine(controller)
-         #lost.init( using_pi )
-         #lost.mainLoop()
-         
-    except Exception as e:
-        tb = traceback.format_exc()
-        print ('^C received, shutting down the web server: ', tb)
-         #sock_comms.stop()
-         #threadSock.join()
-    #threadSock.join()
-    #threadUI.join()
-         #server.stop()
-         #thread.join()
+            #start web server in separate thread
+            self.server = webserver(self.port, self.controller)
+            thread = Thread(target=self.server.start, args=[])
+            thread.start()
 
+            #sock_comms = socket_comms( '/tmp/uds_socket' );
+            #threadSock = Thread(target = sock_comms.start, args = [])
+            #threadSock.start()
+            self.lost = SwanEngine(self.controller, self.quitApp)
+            self.lost.init(self.using_pi)
+            #threadUI = Thread(target = lost.init, args = [ using_pi ])
+            #threadUI.start()
+
+
+             # start lost engine
+             #lost = lostengine(controller)
+             #lost.init( using_pi )
+             #lost.mainLoop()
+
+        except Exception as e:
+            tb = traceback.format_exc()
+            print('^C received, shutting down the web server: ', tb)
+             #sock_comms.stop()
+             #threadSock.join()
+        #threadSock.join()
+        #threadUI.join()
+             #server.stop()
+             #thread.join()
+
+    def quitApp(self, ctrl_c=False):
+        print("quitApp")
+        if ctrl_c:
+            self.lost.stop()
+        #sock_comms.stop()
+        #threadSock.join()
+        print("socket done")
+        self.server.stop()
+
+        #threadUI.join()
+        #server.stop()
+        #thread.join()
+
+
+swan = None
 
 def signal_handler(sig, frame):
     print("CTRL-C")
-    
-    lost.stop()
-    
-    quitApp()
-
-
-def quitApp():
-    print("quitting")
-    sock_comms.stop()
-    threadSock.join()
-    print("socket done");
-        
-    #threadUI.join()
-    #server.stop()
-    #thread.join()
+    swan.quitApp(True)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    swan = Swan(sys.argv[1:])
+    signal.signal(signal.SIGINT, signal_handler)
+    swan.start()
